04fed0ed739a3f9fbb11c69956a3f7d1a261aff4
rp360fr <matthiast@terrot.net>
2026-04-27 17:56:58 +0200

    sa saute

--- Game/Scenes/Runner/RunnerScene.cpp
+++ Game/Scenes/Runner/RunnerScene.cpp
@@ -7,6 +7,38 @@
 #include "Texture.h"
 #include "WindowModule.h"
 
+#include "Demo/Player.h"
+#include "Runner/Player.h"
+#include "Runner/CollisionManager.h"
+#include <iostream>
+
+
+
+
+
 Runner::RunnerScene::RunnerScene() : Scene("Scene courir") {
 
+	AssetsModule* asset = Engine::GetInstance()->GetModuleManager()->GetModule<AssetsModule>();
+
+	Texture* player_texture = asset->LoadAsset<Texture>("logo.png");
+
+	GameObject* player = CreateGameObject("player");
+	GameObject* obj = CreateGameObject("obj");
+
+
+
+	player->SetPosition(Maths::Vector2f(300, 150));
+	player->CreateComponent<Player>();
+	SpriteRenderer* render = player->CreateComponent<SpriteRenderer>(player_texture);
+	player->CreateComponent<SquareCollider>(render);
+	player->CreateComponent<CollisionManager>();
+
+
+	
+	RectangleShapeRenderer* rec =  obj->CreateComponent<RectangleShapeRenderer>();
+	obj->SetPosition(Maths::Vector2f(0, 500));
+	rec->SetSize(Maths::Vector2f(800, 20));
+	rec->SetColor(sf::Color::Red);
+	SquareCollider* coll = obj->CreateComponent<SquareCollider>();
+	coll->SetSize(Maths::Vector2f(800, 20));
 }
---
+++ Game/include/Runner/CollisionManager.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Core/Component.h"
+#include "Maths/Vector2.h"
+
+class SquareCollider;
+class GameObject;
+
+namespace Runner {
+    class Player; // forward declaration, pas d'include
+
+    class CollisionManager : public Component
+    {
+    public:
+        void Start() override;
+        void Update(float deltatime) override;
+
+    private:
+        SquareCollider* my_collider = nullptr;
+        SquareCollider* obj_collider = nullptr;
+        GameObject* obj = nullptr;
+        Player* p = nullptr;
+        GameObject* player;
+
+        Maths::Vector2f velocity;
+        Maths::Vector2f position;
+        bool isOnGround = false;
+
+        const float gravity = 500.f;
+        const float jumpForce = -300.f; // négatif car Y vers le bas en SFML
+    };
+}
